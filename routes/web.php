526210a0755e03831fbec20ce17fdf76e799941d
Rizaldie78 <muhamadrizaldie@gmail.com>
2023-08-14 21:21:13 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+ 
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+Route::get('ajax-crud-datatable', [EmployeeController::class, 'index']);
+Route::post('store', [EmployeeController::class, 'store']);
+Route::post('edit', [EmployeeController::class, 'edit']);
+Route::post('delete', [EmployeeController::class, 'destroy']);
